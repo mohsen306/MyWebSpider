65215f539a9842bd415bc9ea5ef2a770de6c76b7
Mohsen Someh <moh3n.niknezhad@live.com>
2018-10-27 15:13:55 +0330

    ری فرمت

--- page_spider.py
+++ page_spider.py
@@ -1,17 +1,18 @@
-#auther mohsen niknezhad
-import  os
+# auther mohsen niknezhad
+import os
 import argparse
+
 
 def main(database: str, url_list_file: str):
     print("we are going work with" + database)
     print("we are going scan" + url_list_file)
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-db","--database", help="Sqlite File name")
-    parser.add_argument("-i","--input", help="File contianing urls to read")
+    parser.add_argument("-db", "--database", help="Sqlite File name")
+    parser.add_argument("-i", "--input", help="File contianing urls to read")
     args = parser.parse_args()
     database_file = args.database
     input_file = args.input
-    main(database=database_file,url_list_file=input_file)
-
+    main(database=database_file, url_list_file=input_file)
