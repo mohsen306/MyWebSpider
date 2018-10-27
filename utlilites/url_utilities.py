4d0d6123a4cb650449951eb8bedf4057d33cefb6
Mohsen Someh <moh3n.niknezhad@live.com>
2018-10-27 14:53:33 +0330

    کدهای اولیه

---
+++ utlilites/url_utilities.py
@@ -0,0 +1,11 @@
+def load_urls_from_files(file_path: str):
+    # TODO:add the code needed code to read the text file with the urls in it
+    pass
+
+def load_page(url: str):
+    #TODO: add the code that reads the url (from internet)
+    pass
+
+def scrape_page(page_contents: str):
+    #TODO: analayze the text
+    pass
