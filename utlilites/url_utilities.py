acc8a8666176fb58369861f44746f8722d87e86f
Mohsen Someh <moh3n.niknezhad@live.com>
2018-10-27 15:14:07 +0330

    ری فرمت

--- utlilites/url_utilities.py
+++ utlilites/url_utilities.py
@@ -2,10 +2,12 @@ def load_urls_from_files(file_path: str):
     # TODO:add the code needed code to read the text file with the urls in it
     pass
 
+
 def load_page(url: str):
-    #TODO: add the code that reads the url (from internet)
+    # TODO: add the code that reads the url (from internet)
     pass
 
+
 def scrape_page(page_contents: str):
-    #TODO: analayze the text
+    # TODO: analayze the text
     pass
