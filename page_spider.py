e09e67072b564f695eeb059c0205da4b54005f93
Mohsen Someh <moh3n.niknezhad@live.com>
2018-10-28 09:10:05 +0330

    خواندن اطلاعات از url_utilities  و نمایش آنها

--- page_spider.py
+++ page_spider.py
@@ -1,11 +1,19 @@
 # auther mohsen niknezhad
 import os
 import argparse
-
+from utlilites import url_utilities
 
 def main(database: str, url_list_file: str):
+    big_word_list = []
     print("we are going work with" + database)
     print("we are going scan" + url_list_file)
+    urls = url_utilities.load_urls_from_files(url_list_file)
+    for url in urls:
+        print("reading "+ url)
+        page_content = url_utilities.load_page(url=url)
+        words = url_utilities.scrape_page(page_contents=page_content)
+        big_word_list.extend(words)
+        print(big_word_list)
 
 
 if __name__ == "__main__":
